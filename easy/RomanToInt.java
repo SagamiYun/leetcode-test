e84645c15260d0e6486f7e059e2073653a9ebc28
SagamiYum <qiaolei19981021@163.com>
2023-02-03 04:47:45 +0800

    feat(global): easy update 02/03

---
+++ easy/RomanToInt.java
@@ -0,0 +1,52 @@
+package easy;
+
+/**
+ * @author SagamiYun
+ * @version 2023/2/3
+ * <p>@ClassName RomanToInt<p/>
+ * <p>@Description 这段代码背后的逻辑是从左到右遍历罗马数字字符串，
+ * 并根据每个符号的值是否大于或小于前一个符号将其添加或减去结果。 <p/>
+ * <p>13. 罗马数字转整数<p/>
+ */
+public class RomanToInt {
+    public int romanToInt(String s) {
+        // 罗马数字的一个规律，第x位若比第x+1位小，则为减法，若x位比x+1位大，则为加法。
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int result = 0;
+        int prev = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int curr = getValue(s.charAt(i));
+            if (prev < curr) {
+                result -= prev;
+            } else {
+                result += prev;
+            }
+            prev = curr;
+        }
+        result += prev;
+        return result;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
--- easy/TestMain.java
+++ easy/TestMain.java
@@ -15,7 +15,16 @@ public class TestMain {
         // System.out.println(leftRe());
 
         // 两数之和
-        System.out.println(Arrays.toString(twoNumSun()));
+        // System.out.println(Arrays.toString(twoNumSun()));
+
+        // 回文数
+        // System.out.println(testPalindrome());
+
+        // 罗马数字转数字
+        // System.out.println(testRomanToInt());
+
+        // 最长公共前缀
+        System.out.println(testLongestCommonPrefix());
     }
 
     private static String leftRe(){
@@ -29,5 +38,22 @@ public class TestMain {
         int[] nums = {3, 2, 4};
         int target = 6;
         return twoSum.twoSum(nums, target);
+    }
+
+    private static boolean testPalindrome() {
+        int num = 1222231;
+        return Palindrome.isPalindrome(num);
+    }
+
+    private static int testRomanToInt() {
+        RomanToInt romanToInt = new RomanToInt();
+        String s = "MCMXCIV";
+        return romanToInt.romanToInt(s);
+    }
+
+    private static String testLongestCommonPrefix() {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String[] strs = {"flower","flow","flight"};
+        return longestCommonPrefix.longestCommonPrefix(strs);
     }
 }
